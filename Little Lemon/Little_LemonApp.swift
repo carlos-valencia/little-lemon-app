0269ee18b75450537eb7b80dcc918f579e12a846
Carlos <carlossebastian2303@hotmail.com>
2024-10-11 18:48:50 -0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Carlos Valencia on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Little LemonTests/Little_LemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonTests.swift
+//  Little LemonTests
+//
+//  Created by Carlos Valencia on 10/11/24.
+//
+
+import Testing
+@testable import Little_Lemon
+
+struct Little_LemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
