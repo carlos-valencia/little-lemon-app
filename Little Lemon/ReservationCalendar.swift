8de2c3aa72d1302187cd4b97df9b01b332a8be71
carlos-valencia <carlossebastian2303@hotmail.com>
2024-10-17 19:50:42 -0400

    Edited ReservationCalendar for exercise

--- Little Lemon/ReservationCalendar.swift
+++ Little Lemon/ReservationCalendar.swift
@@ -9,22 +9,24 @@ import SwiftUI
 
 struct ReservationCalendar: View {
     
-    @State var selectedDate = Date()
+    @State var reservationDate = Date()
     
     var body: some View {
         Form {
             HStack {
                 DatePicker(
-                    selection: $selectedDate,
-                    label: { Text("Reservation Date") })
-                Button(
-                    action: {
-                        
-                    }, label: {
+                    selection: $reservationDate, in: Date()...,
+                    displayedComponents: [.date, .hourAndMinute]
+                ) {}
+                Button(action: { print("do something") }) {
+                    HStack {
+                        Image(systemName: "arrow.right.circle")
                         Text("Done")
-                    })
+                    }
+                }.padding(20)
                 
             }
+            Text("Reservation date is \(reservationDate.formatted(date: .long, time: .complete))")
         }
     }
 }
