61151898e81a041935029e28ee52ea568f7174c3
carlos-valencia <carlossebastian2303@hotmail.com>
2024-10-23 10:04:05 -0400

    Built basic reservation form

---
+++ Little Lemon/ReservationFormView.swift
@@ -0,0 +1,66 @@
+//
+//  ReservationFormView.swift
+//  Little Lemon
+//
+//  Created by Carlos Valencia on 10/23/24.
+//
+
+import SwiftUI
+import Combine
+
+struct ReservationFormView: View {
+    
+    @State private var name: String = ""
+    @State private var numberOfGuests: Int?
+    @State private var reservationDate = Date()
+    @State private var reservationTime = Date()
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let startComppnents = DateComponents()
+        let endComponents = DateComponents()
+        return calendar.date(from: startComppnents)!
+            ...
+            calendar.date(from: endComponents)!
+    }()
+    
+    var body: some View {
+        Form {
+            VStack {
+                
+                HStack {
+                    DatePicker("Reservation date", selection: $reservationDate, displayedComponents: [.date])
+                    Button(action: {
+                        print("checking availability")
+                    }) {
+                        Image(systemName: "")
+                    }
+                }
+                HStack {
+                    Text("Enter your name:")
+                    Spacer()
+                    TextField("Enter your name", text: $name, prompt: Text("Required"))
+                }
+                
+                HStack {
+                    Text("Number of guests")
+                    Spacer()
+                    TextField("Enter number of guests", value: $numberOfGuests, format: .number, prompt: Text("Required"))
+                        .keyboardType(.numberPad)
+                }
+                
+                HStack {
+                    DatePicker("Reservation time", selection: $reservationTime, displayedComponents: [.hourAndMinute])
+                }
+                
+                Button("Confirm Reservation") {
+                    print("reservation confirmed")
+                }
+            }
+            .padding(20)
+        }
+    }
+}
+
+#Preview {
+    ReservationFormView()
+}
